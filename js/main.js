5b67fd54e251e1af61cbff718dd7ec1b8d9e04f9
deborahtrota <deborahtrota@gmail.com>
2021-01-22 10:25:29 -0300

    responsividade em todas as telas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-document.querySelector(".submit").onclick = function checarEmail(){
+//Função para validar o input
+
+document.querySelector(".submit").onclick = function validateInput(){
 if( document.querySelector("input").value==""
    || document.querySelector("input").value.indexOf('@')==-1
      || document.querySelector("input").value.indexOf('.')==-1 )
@@ -10,17 +12,3 @@ if( document.querySelector("input").value==""
 	  return false;
 	}
 }
-
-/* Ativar função ao pressionar enter 
-
-var keyElement = document.querySelector('.submit');
-keyElement.addEventListener("onkeypress", keyFunction(e));
-
-function keyFunction(e){
-  var key = e.keyCode;
-  if (key == 13) {
-    alert("sopa");
-  }
-}
-
-*/
